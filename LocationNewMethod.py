f93c3ae0bffde7eb88159635b7cea23fc34e4535
Quinten <quinten.nieuwhof@hotmail.com>
2024-05-08 23:41:49 +0200

    Channel 3

--- LocationNewMethod.py
+++ LocationNewMethod.py
@@ -26,19 +26,19 @@ class localization:
         audio_channel3 = audiowav[:,2]
         audio_channel4 = audiowav[:,3]
         audio_channel5 = audiowav[:,4]
-        ref_signalx = wavaudioread("opnames/reference.wav", Fs_RX)
-        ref_signaly =  ref_signalx[:,0]
+        Fs, ref_signal = wavfile.read("opnames/reference.wav")
+        ref_signal =  ref_signal[:,0]
         
         
-        fig, ax = plt.subplots(2, 1, figsize=(20,10))
-        ax[0].plot(audio_channel1, color='C0')
-        ax[0].set_title("ref")
-        ax[0].set_xlabel("Time [s]")
-        ax[0].set_ylabel("Amplitude")
-        fig.tight_layout()
-        #plt.show()
-        plt.savefig('channel1', dpi=300)
-        plt.close()
+        # fig, ax = plt.subplots(2, 1, figsize=(20,10))
+        # ax[0].plot(audio_channel1, color='C0')
+        # ax[0].set_title("ref")
+        # ax[0].set_xlabel("Time [s]")
+        # ax[0].set_ylabel("Amplitude")
+        # fig.tight_layout()
+        # #plt.show()
+        # plt.savefig('channel1', dpi=300)
+        # plt.close()
         
         
         #segments
@@ -47,18 +47,24 @@ class localization:
         segments_channel3 = localization.detect_segments(audio_channel3)
         segments_channel4 = localization.detect_segments(audio_channel4)
         segments_channel5 = localization.detect_segments(audio_channel5)
-        ref_signalz = localization.detect_segments(ref_signaly)
-        ref_signal =  ref_signalz[5]
-        
-        fig, ax = plt.subplots(2, 1, figsize=(20,10))
-        ax[0].plot(segments_channel1[5], color='C0')
-        ax[0].set_title("ref")
-        ax[0].set_xlabel("Time [s]")
-        ax[0].set_ylabel("Amplitude")
-        fig.tight_layout()
-        #plt.show()
-        plt.savefig('segment.png', dpi=300)
-        plt.close()
+        refsig = localization.detect_segments(ref_signal)
+        ref = refsig[12]
+
+        plt.figure(figsize=(20,10))
+        plt.plot(segments_channel1[5], color='C0')
+        plt.title("ref")
+        plt.xlabel("Time [s]")
+        plt.ylabel("Amplitude")
+        plt.grid(True)
+        plt.show()
+
+        plt.figure(figsize=(20,10))
+        plt.plot(abs(ref), color='C0')
+        plt.title("ref")
+        plt.xlabel("Time [s]")
+        plt.ylabel("Amplitude")
+        plt.grid(True)
+        plt.show()
         
         #channel estimation
         
@@ -68,91 +74,95 @@ class localization:
         #TOT HIER IS OUTPUT!!! gaat dus iets mis in ch3
         
         
-        ch_audio_channel1 = localization.ch3(segments_channel1[5], ref_signal)
-        ch_audio_channel2 = localization.ch3(segments_channel2[5], ref_signal)
-        ch_audio_channel3 = localization.ch3(segments_channel3[5], ref_signal)
-        ch_audio_channel4 = localization.ch3(segments_channel4[5], ref_signal)
-        ch_audio_channel5 = localization.ch3(segments_channel5[5], ref_signal)
+        # ch_audio_channel1 = localization.ch3(segments_channel1[5], refsig[12])
+        # ch_audio_channel2 = localization.ch3(segments_channel2[5], refsig[12])
+        # ch_audio_channel3 = localization.ch3(segments_channel3[5], refsig[12])
+        # ch_audio_channel4 = localization.ch3(segments_channel4[5], refsig[12])
+        # ch_audio_channel5 = localization.ch3(segments_channel5[5], refsig[12])
+
+        channel_responses_1 = [localization.ch3(segment, ref) for segment in segments_channel1]
+        channel_responses_array_1 = np.array(channel_responses_1)
+
+        channel_responses_4 = [localization.ch3(segment, ref) for segment in segments_channel4]
+        channel_responses_array_4 = np.array(channel_responses_4)
         
-        fig, ax = plt.subplots(2, 1, figsize=(20,10))
-        ax[0].plot(ch_audio_channel1[5], color='C0')
-        ax[0].set_title("ref")
-        ax[0].set_xlabel("Time [s]")
-        ax[0].set_ylabel("Amplitude")
-        fig.tight_layout()
-        #plt.show()
-        plt.savefig('ch1_.png', dpi=300)
-        plt.close()
+        plt.figure(figsize=(20,10))
+        plt.plot(channel_responses_array_1[1], color='C0')
+        plt.title("ref")
+        plt.xlabel("Time [s]")
+        plt.ylabel("Amplitude")
+        plt.grid(True)
+        plt.show()
         
         
         #peaks
-        peaks_channel1 = localization.find_segment_peaks(ch_audio_channel1, peak_threshold = 0.9*np.max(segments_channel1))
-        peaks_channel2 = localization.find_segment_peaks(ch_audio_channel2, peak_threshold = 0.9*np.max(segments_channel2))
-        peaks_channel3 = localization.find_segment_peaks(ch_audio_channel3, peak_threshold = 0.9*np.max(segments_channel3))
-        peaks_channel4 = localization.find_segment_peaks(ch_audio_channel4, peak_threshold = 0.9*np.max(segments_channel4))
-        peaks_channel5 = localization.find_segment_peaks(ch_audio_channel5, peak_threshold = 0.9*np.max(segments_channel5))   
+        peaks_channel1 = localization.find_segment_peaks(channel_responses_array_1)
+        # peaks_channel2 = localization.find_segment_peaks(ch_audio_channel2, peak_threshold = 0.9*np.max(segments_channel2))
+        # peaks_channel3 = localization.find_segment_peaks(ch_audio_channel3, peak_threshold = 0.9*np.max(segments_channel3))
+        peaks_channel4 = localization.find_segment_peaks(channel_responses_array_4)
+        # peaks_channel5 = localization.find_segment_peaks(ch_audio_channel5, peak_threshold = 0.9*np.max(segments_channel5))   
         
-        print(peaks_channel1)
-        print(peaks_channel2)
-        print(peaks_channel3)
-        print(peaks_channel4)
-        print(peaks_channel5)
+        #print(peaks_channel1)
+        # print(peaks_channel2)
+        # print(peaks_channel3)
+        # print(peaks_channel4)
+        # print(peaks_channel5)
+
+        sorted_peaks_1 = np.sort(peaks_channel1)
+        trimmed_peaks_1 = sorted_peaks_1[10:-10]
+        mean_peak_1 = np.mean(trimmed_peaks_1)
+
+        print(mean_peak_1)
+
+        sorted_peaks_4 = np.sort(peaks_channel4)
+        trimmed_peaks_4 = sorted_peaks_4[10:-10]
+        mean_peak_4 = np.mean(trimmed_peaks_4)
+
+        print(mean_peak_4)
          
-        print("Number of segments with peaks:", len(peaks_channel1))
-        print("Number of segments with peaks:", len(peaks_channel4))
+        # print("Number of segments with peaks:", len(peaks_channel1))
+        # print("Number of segments with peaks:", len(peaks_channel4))
 
         # Calculate TDOA between different microphone pairs
-        TDOA14 = localization.TDOA(peaks_channel1, peaks_channel4)
+        TDOA14 = localization.TDOA(mean_peak_1, mean_peak_4)
         
         print(TDOA14)
         
         
-        x=5
-        y=5
+        # x=5
+        # y=5
         
-        return x, y
+        # return x, y
     
     def detect_segments(audio_signal):
         segments = []
         num_segments = 40
         segment_length = len(audio_signal) // num_segments
-        print(len(audio_signal))
-        print(segment_length)
+        #print(len(audio_signal))
+        #print(segment_length)
         segments = [audio_signal[i*segment_length : (i+1)*segment_length] for i in range(num_segments)]
-        
+
         return segments    
 
-    def find_segment_peaks(segment_signal, peak_threshold):
+    def find_segment_peaks(segment_signal):
         peaks_list = []
         for segment in segment_signal:
             peaks = np.argmax(segment)
             peaks_list.append(peaks)
-            
-        fig, ax = plt.subplots(2, 1, figsize=(20,10))
-        ax[0].plot(segment_signal[1], color='C0')
-        ax[0].plot(peaks_list[1], 0, color='green', marker='o', markersize=10, label="Detected Peak")
-        ax[0].set_title("Estimation of recording")
-        ax[0].set_xlabel("Time [s]")
-        ax[0].set_ylabel("Amplitude")
-
-        
-        fig.tight_layout()
-        #plt.show()
-        plt.savefig('plot_full_.png', dpi=300)
-        plt.close()
-        
-        
+              
         return peaks_list
     
-    def TDOA(segment_peaks1, segment_peaks2):
-        tdoa_values = []
-        for peak1, peak2 in zip(segment_peaks1, segment_peaks2):
-            # Calculate TDOA for the current segment
-            tdoa = (peak2 - peak1) / Fs_RX *343
-            tdoa_values.append(tdoa)
-        print(tdoa_values)
+    def TDOA(peak1, peak2):
+        # tdoa_values = []
+        # for peak1, peak2 in zip(segment_peaks1, segment_peaks2):
+        #     # Calculate TDOA for the current segment
+        #     tdoa = (peak2 - peak1) / Fs_RX *343
+        #     tdoa_values.append(tdoa)
+        # print(tdoa_values)
     
-        mean_tdoa = np.mean(tdoa_values)
+        # mean_tdoa = np.mean(tdoa_values)
+
+        mean_tdoa = (peak2 - peak1)/Fs * 343
         return mean_tdoa
 
             
@@ -327,7 +337,7 @@ if __name__ == "__main__":
 # Localize the sound source
 # Present the results
     localizer = localization()
-    Fs_RX, ABS1 = wavfile.read("opnames/record_x64_y40.wav")
+    Fs, ABS1 = wavfile.read("opnames/record_x64_y40.wav")
     # ABS2 = wavaudioread("opnames/record_x82_y399.wav", Fs_RX)
     # ABS3 = wavaudioread("opnames/record_x109_y76.wav", Fs_RX)
     # ABS4 = wavaudioread("opnames/record_x143_y296.wav", Fs_RX)
@@ -337,8 +347,8 @@ if __name__ == "__main__":
     # ABS8 = wavaudioread("opnames/record_x4_y_hidden_1.wav", Fs_RX)
     # ABS9 = wavaudioread("opnames/record_x_y_hidden_2.wav", Fs_RX)
     # ABS10 = wavaudioread("opnames/record_x_y_hidden_3.wav", Fs_RX)
-    refsig = wavaudioread("opnames/reference.wav", Fs_RX)
-    FTrefsig = fft(refsig)
+    #refsig = wavaudioread("opnames/reference.wav", Fs_RX)
+    #FTrefsig = fft(refsig)
 
     x_car1, y_car1 = localization.localization(ABS1)
     #x_car2, y_car2 = localization.localization(ABS2)
@@ -353,7 +363,7 @@ if __name__ == "__main__":
 
 
 
-    print(x_car1, y_car1)
+    #print(x_car1, y_car1)
     #print(x_car2, y_car2)
     #print(x_car3, y_car3)
     #print(x_car4, y_car4)
