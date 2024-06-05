9762e5a8d2bb82bc5f4d06f366c26963aee0e324
souped <jorispost@me.com>
2024-06-05 12:12:47 +0200

    introduce statetracker

---
+++ statetracker.py
@@ -0,0 +1,13 @@
+class State:
+    def __init__(self) -> None:
+        self.xy = [0, 0]
+        self.direction = [0, 1]
+
+    def determine_dir(self):
+        # determine direction and xy by driving forwards a small amount
+        pass
+
+    
+
+
+    
