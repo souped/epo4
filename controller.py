4854006d994b0bb237f7a76c2bb0997afedfc794
souped <jorispost@me.com>
2024-06-10 12:26:26 +0200

    os check

--- controller.py
+++ controller.py
@@ -10,8 +10,12 @@ from Routeplanner import RoutePlanner
 from KITT_communication import KITT
 from Keyboard import Keyboard
 from StateTracker import StateTracker
+import os
 
-sysport = 'COM5'
+if os.name == 'nt': # windows:
+    sysport = 'COM4'
+elif os.name == 'posix':
+    sysport = '/dev/cu.RNBT-3F3B'
 CHANNELS = 8
 RATE = 48000
 
@@ -36,24 +40,25 @@ class Controller():
 
         self.state = StateTracker(self.kitt, self.md, self.localizer, self.mic, self.ref)
 
-    def run_loop(self, dest, carloc=(0.0,0.0), car_rad=0.5*np.pi):
+    def run_loop(self, dest, carloc=(0.73,0.92), car_rad=0.5*np.pi):
         # while self.running is True:
         # apply route planning algorithm?
         curve_cmd, model_endpos, model_dir = self.rp.make_curve_route(carloc, car_rad, dest)
-        Keyboard.car_model_input(kitt=self.kitt, input_cmd=curve_cmd, from_curve=1)
+        Keyboard.car_model_input(kitt=self.kitt, input_cmd=curve_cmd)
 
-        # state, (x,y), dir = self.state.after_curve_deviation(model_endpos=model_endpos, model_dir=model_dir, dest=dest)
+        state, (x,y), dir = self.state.after_curve_deviation(model_endpos=model_endpos, model_dir=model_dir, dest=dest)
         # while state == 0:
         #     curve_cmd_corr,model_endpos,model_dir=self.rp.make_curve_route((x,y),dir,dest)
-        #     Keyboard.car_model_input(kitt=self.kitt, input_cmd=curve_cmd_corr, from_curve=1)
+        #     Keyboard.car_model_input(kitt=self.kitt, input_cmd=curve_cmd_corr)
         #     state,(x,y),dir=self.state.after_curve_deviation(model_endpos=model_endpos,model_dir=model_dir,dest=dest)
 
         print("Generating straight commands...")
-        straight_cmd = self.rp.make_straight_route(model_endpos, dest)
+        straight_cmd = self.rp.make_straight_route(carloc, dest)
         Keyboard.car_model_input(kitt=self.kitt, input_cmd=straight_cmd)
 
-        # print("Final location:", self.state.determine_location())
-
+        self.kitt.start_beacon()
+        print("Final location:", self.state.determine_location())
+        self.kitt.stop_beacon()
         # track data?
         # do this inside the KITT Class or a separate other class i.e. DrivingHistory
 
@@ -63,17 +68,8 @@ class Controller():
         # temporary end
         self.running = False
 
-    def TDOA_tester(self):
-        input("Place car on the field, press Enter to continue...")
-        i=0
-        while i<4:
-            x,y = self.state.determine_location()
-            print("Current location:",x,y)
-            input("Move car to next location, press Enter to continue...")
-            i+=1
-
 
 if __name__ == "__main__":
     controller = Controller()
-    controller.run_loop(carloc=(0,2.3), car_rad=1.3*np.pi, dest=())
+    controller.run_loop((3,0))
     print(1)
